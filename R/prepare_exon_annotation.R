f8af92342216500f933bf47427498662908c819e
luciorq <luciorqueiroz@gmail.com>
2023-03-15 14:49:37 -0400

    feat: create prepare_exon_annotation function

---
+++ R/prepare_exon_annotation.R
@@ -0,0 +1,61 @@
+#' Prepare Exon based Position Annotation Table
+#' @param gene_names String or vector of gene names to extract.
+#' @param file_path Path to annotation file.
+#' @inheritParams download_reference
+#' @export
+prepare_exon_annotation <- function(gene_name,
+                                    file_path,
+                                    file_type = "gff") {
+  if (isTRUE(file_type == "gff")) {
+    gff_file_path <- fs::path(file_path)
+    annot_df <- readr::read_table(
+      file = gff_file_path,
+      col_names = FALSE,
+      col_types = readr::cols(
+        X1 = readr::col_skip(),
+        X2 = readr::col_skip(),
+        X3 = "c",
+        X4 = "c",
+        X5 = "c",
+        X6 = readr::col_skip(),
+        X7 = "c",
+        X8 = readr::col_skip(),
+        X9 = "c"
+      ),
+      comment = "#",
+      progress = FALSE
+    )
+  }
+
+  gene_annot_df <- annot_df |>
+    dplyr::filter(
+      stringr::str_detect(X9, paste0("gene_name=", gene_name, ";"))
+    )
+  tx_id_vector <- gene_annot_df |>
+    dplyr::filter(X3 %in% "transcript") |>
+    dplyr::select(X9) |>
+    dplyr::mutate(
+      tx_id = stringr::str_extract(X9, "transcript_id=.*?;") |>
+        stringr::str_remove("^transcript_id=") |>
+        stringr::str_remove(";")
+    ) |>
+    dplyr::select(tx_id) |>
+    dplyr::arrange() |>
+    dplyr::distinct() |>
+    dplyr::pull(tx_id)
+
+  # parent_id <- tx_id_vector[1]
+  tx_id_vector |>
+    purrr::map_dfr(
+      .f = \(parent_id) {
+        gene_annot_df |>
+          dplyr::filter(
+            X3 %in% "exon",
+            stringr::str_detect(X9, glue::glue("Parent={parent_id};"))
+          ) |>
+          dplyr::select(c(X4, X5, X7)) |>
+          dplyr::mutate(tx_id = parent_id)
+      }
+    ) |>
+    dplyr::mutate(tx_name = gene_name)
+}
