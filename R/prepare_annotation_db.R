6886246a51575f96eca79c3fd7a992fdf50194d3
luciorq <luciorqueiroz@gmail.com>
2025-10-04 15:40:51 -0400

    feat: new duckdb based prepare_annotation

---
+++ R/prepare_annotation_db.R
@@ -0,0 +1,55 @@
+#' Write Parquet File from GFF
+prepare_annotation_db <- function(
+  input_path,
+  output_path = NULL,
+  file_type = c("gff")
+) {
+  file_type <- stringr::str_to_lower(file_type)
+  file_type <- rlang::arg_match(file_type)
+  if (isTRUE(rlang::is_null(output_path) || identical(output_path, ""))) {
+    parquet_file_path <- "none"
+  } else {
+    parquet_file_path <- fs::path(
+      fs::path_ext_remove(output_path), ext = "parquet"
+    )
+  }
+
+  if (!isTRUE(fs::file_exists(input_path))) {
+    cli::cli_abort(
+      message = c(
+        x = "{.path {input_path}} do {.strong not} exist."
+      ),
+      class = "isoformic_annot_file_dont_exist"
+    )
+  }
+
+  sql_str <- r"---(SET enable_progress_bar = false;
+COPY (
+SELECT t.seqid, t.source, t.type, t.start_pos,
+  t.end_pos, t.score, t.strand, t.phase,
+  split_part(regexp_split_to_table(t.attributes, ';'), '=', 1) AS key,
+  split_part(regexp_split_to_table(t.attributes, ';'), '=', 2) AS value
+FROM (
+  SELECT *
+  FROM read_csv(
+    {`input_path`},
+    delim = '\t', comment = '#',
+    encoding='utf-8',
+    header = false,
+    names = [
+      'seqid', 'source', 'type', 'start_pos', 'end_pos',
+      'score', 'strand', 'phase', 'attributes'
+    ]
+  )
+  WHERE type IN ('gene', 'transcript', 'exon')
+) t
+) TO {`parquet_file_path`} (FORMAT parquet, COMPRESSION zstd);
+)---"
+  duckdb_run(
+    sql_string = sql_str,
+    db_type = "duckdb_memory",
+    db_file_path = NULL,
+    read_only = TRUE
+  )
+  return(invisible(parquet_file_path))
+}
